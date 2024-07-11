dc959d63847f1173dc7cce14e0e4a364de8460fe
Paschal Ugoh <paskyugoh@gmail.com>
2024-07-11 14:53:27 +0100

    02-express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.post('/login', (req, res) => {
+    const {name} = req.body
+
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(404).send("Please enter a name")
+})
+
+app.listen(5000, () => {
+    console.log('Port is runing on port 5000')
+})
